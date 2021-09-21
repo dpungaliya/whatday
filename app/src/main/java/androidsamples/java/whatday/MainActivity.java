eb4abe54c9b8ceeb5c5ac7dcb77672c004391949
Dhaval Prashant Pungaliya <dhaval.pungaliya@gmail.com>
2021-09-21 15:58:26 +0530

    2nd commit

--- app/src/main/java/androidsamples/java/whatday/MainActivity.java
+++ app/src/main/java/androidsamples/java/whatday/MainActivity.java
@@ -8,6 +8,9 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
   private EditText dateInput;
@@ -15,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
   private EditText yearInput;
   private Button checkOutput;
   private TextView txtOutput;
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,58 @@ public class MainActivity extends AppCompatActivity {
     monthInput = findViewById(R.id.editMonth);
     yearInput = findViewById(R.id.editYear);
     checkOutput = findViewById(R.id.btn_check);
+    txtOutput = findViewById(R.id.txt_display);
+
+    checkOutput.setOnClickListener(view->{
+      int dateInt = Integer.parseInt(dateInput.getText().toString());
+      int monthInt = Integer.parseInt(monthInput.getText().toString());
+      int yearInt = Integer.parseInt(yearInput.getText().toString());
+
+      
+//      monthInt--;
+//      int flag = 0;
+//
+//      if(dateInt < 0 || dateInt > 31)
+//        {
+//          txtOutput.setText("Invalid Date");
+//          flag=-1;
+//        }
+//      else if(monthInt < 0 || monthInt > 12)
+//        {
+//          txtOutput.setText("Invalid Month");
+//          flag=-1;
+//        }
+//
+//      Calendar rightNow = Calendar.getInstance();
+//      rightNow.set(yearInt, monthInt, dateInt);
+//      Date currentDate = rightNow.getTime();
+//      int currentDay = currentDate.getDay();
+//      //int min = rightNow.getMinimum(Calendar.YEAR);
+//      String dayText;
+//      System.out.println("Hello" + dateInt);
+//      switch (currentDay){
+//        case 0: dayText = "Sunday";
+//          break;
+//        case 1: dayText = "Monday";
+//          break;
+//        case 2: dayText = "Tuesday";
+//          break;
+//        case 3: dayText = "Wednesday";
+//          break;
+//        case 4: dayText = "Thursday";
+//          break;
+//        case 5: dayText = "Friday";
+//          break;
+//        case 6: dayText = "Saturday";
+//          break;
+//        default:
+//          throw new IllegalStateException("Unexpected value: " + currentDay);
+//      }
+//        if(flag!=-1)
+//          txtOutput.setText(dayText);
+//
+
+    });
 
 
   }

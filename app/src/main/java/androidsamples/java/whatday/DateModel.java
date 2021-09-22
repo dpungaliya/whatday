0d5c9492c8a16a82a9df514f1879b4058a252616
Dhaval Prashant Pungaliya <dhaval.pungaliya@gmail.com>
2021-09-22 12:12:26 +0530

    third comit

--- app/src/main/java/androidsamples/java/whatday/DateModel.java
+++ app/src/main/java/androidsamples/java/whatday/DateModel.java
@@ -1,11 +1,16 @@
 package androidsamples.java.whatday;
 
 import androidx.annotation.NonNull;
+import java.util.Calendar;
+import java.util.Date;
 
 /**
  * Represents the date to be set, whether it is a valid date, and the message with error status or the day of the week.
  */
 public class DateModel {
+
+  static String year,month,date;
+
 
   /**
    * Initializes the {@link DateModel} with the given year, month, and date.
@@ -18,10 +23,12 @@ public class DateModel {
    */
   public static void initialize(String yearStr, String monthStr, String dateStr) {
     // TODO implement the method to match the description
-
-    //check for dates
-
+    year=yearStr;
+    month=monthStr;
+    date=dateStr;
   }
+
+
 
   /**
    * Retrieves the message from the {@link DateModel}.
@@ -33,6 +40,60 @@ public class DateModel {
   @NonNull
   public static String getMessage() {
     // TODO implement the method to match the description
+
+    int dateInt = Integer.parseInt(dateInput.getText().toString());
+    int monthInt = Integer.parseInt(monthInput.getText().toString());
+    int yearInt = Integer.parseInt(yearInput.getText().toString());
+
+    monthInt--;
+      int flag = 0;
+
+      if(dateInt < 0 || dateInt > 31)
+        {
+          txtOutput.setText("Invalid Date");
+          flag=-1;
+        }
+      else if(monthInt < 0 || monthInt > 12)
+        {
+          txtOutput.setText("Invalid Month");
+          flag=-1;
+        }
+
+      Calendar calendar = Calendar.getInstance();
+      calendar.set(Calendar.DAY_OF_MONTH,Integer.parseInt(date));// Day of the month (1-31)
+      calendar.set(Calendar.MONTH,Integer.parseInt(month)-1);// Month (0-11)
+      calendar.set(Calendar.YEAR,Integer.parseInt(year));
+
+      
+//      calendar.set(yearInt, monthInt, dateInt);
+//      Date currentDate = calender.getTime();
+//      int currentDay = currentDate.getDay();
+//      int min = calender.getMinimum(Calendar.YEAR);
+//      String dayText;
+//      System.out.println("Hello" + dateInt);
+//      switch (currentDay){
+//        case 0: dayText = "Sunday";
+//          break;
+//        case 1: dayText = "Monday";
+//          break;
+//        case 2: dayText = "Tuesday";
+//          break;
+//        case 3: dayText = "Wednesday";
+//          break;
+//        case 4: dayText = "Thursday";
+//          break;
+//        case 5: dayText = "Friday";
+//          break;
+//        case 6: dayText = "Saturday";
+//          break;
+//        default:
+//          throw new IllegalStateException("Unexpected value: " + currentDay);
+//      }
+//        if(flag!=-1)
+//          txtOutput.setText(dayText);
+//
+
+
 
 
 
--- app/src/main/java/androidsamples/java/whatday/MainActivity.java
+++ app/src/main/java/androidsamples/java/whatday/MainActivity.java
@@ -32,56 +32,15 @@ public class MainActivity extends AppCompatActivity {
     txtOutput = findViewById(R.id.txt_display);
 
     checkOutput.setOnClickListener(view->{
-      int dateInt = Integer.parseInt(dateInput.getText().toString());
-      int monthInt = Integer.parseInt(monthInput.getText().toString());
-      int yearInt = Integer.parseInt(yearInput.getText().toString());
+      String date = dateInput.getText().toString();
+      String month = monthInput.getText().toString();
+      String year = yearInput.getText().toString();
 
 
-//      monthInt--;
-//      int flag = 0;
-//
-//      if(dateInt < 0 || dateInt > 31)
-//        {
-//          txtOutput.setText("Invalid Date");
-//          flag=-1;
-//        }
-//      else if(monthInt < 0 || monthInt > 12)
-//        {
-//          txtOutput.setText("Invalid Month");
-//          flag=-1;
-//        }
-//
-//      Calendar rightNow = Calendar.getInstance();
-//      rightNow.set(yearInt, monthInt, dateInt);
-//      Date currentDate = rightNow.getTime();
-//      int currentDay = currentDate.getDay();
-//      //int min = rightNow.getMinimum(Calendar.YEAR);
-//      String dayText;
-//      System.out.println("Hello" + dateInt);
-//      switch (currentDay){
-//        case 0: dayText = "Sunday";
-//          break;
-//        case 1: dayText = "Monday";
-//          break;
-//        case 2: dayText = "Tuesday";
-//          break;
-//        case 3: dayText = "Wednesday";
-//          break;
-//        case 4: dayText = "Thursday";
-//          break;
-//        case 5: dayText = "Friday";
-//          break;
-//        case 6: dayText = "Saturday";
-//          break;
-//        default:
-//          throw new IllegalStateException("Unexpected value: " + currentDay);
-//      }
-//        if(flag!=-1)
-//          txtOutput.setText(dayText);
-//
+      DateModel dateObj= new DateModel();
+      DateModel.initialize(year,month,date);
 
     });
-
 
   }
 }
